0a53c8126b16b9b977f3d4b5a47cffe6d7966363
bada <7149348+0xbada@users.noreply.github.com>
2026-03-31 19:54:56 +0900

    feat: add Cloudflare Pages headers config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy({ "src/_headers": "_headers" });
 
   eleventyConfig.addFilter("toolsByCategory", function (tools, categorySlug) {
     return tools.filter((t) => t.category === categorySlug);
